f4278ccc00298a777618232b5089f53519013d2f
ehcodes <engrid.hernandez@gmail.com>
2023-09-19 09:53:48 -0400

    Updated ahtml to include form

--- views/New.jsx
+++ views/New.jsx
@@ -1,13 +1,22 @@
-const React = require("react");
+const React = require('react');
 
 class New extends React.Component {
   render() {
-    // const { fruits } = this.props;
     return (
-      <div>
-        <h1>New Page</h1>
-      </div>
-    );
+        <div>
+            <h1>New Fruit page</h1>
+            {/* 
+            action = server route
+            method = HTTP verb 
+            */}
+            <form action="/fruits" method="POST">
+              Name: <input type="text" name="name" /><br/>
+              Color: <input type="text" name="color" /><br/>
+              Is Ready To Eat: <input type="checkbox" name="readyToEat" /><br/>
+              <input type="submit" name="" value="Create Fruit"/>
+            </form>
+        </div>);
+    }
   }
-}
+
 module.exports = New;
