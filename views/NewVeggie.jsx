fc7346002bbeae5e22ef6752c71f6e9a9283c2a1
ehcodes <engrid.hernandez@gmail.com>
2023-09-19 10:46:33 -0400

    added NewVeggie component

---
+++ views/NewVeggie.jsx
@@ -0,0 +1,22 @@
+const React = require('react');
+
+class NewVeggie extends React.Component {
+  render() {
+    return (
+        <div>
+            <h1>New Veggie page</h1>
+            {/* 
+            action = server route
+            method = HTTP verb 
+            */}
+            <form action="/veggies" method="POST">
+              Name: <input type="text" name="name" /><br/>
+              Color: <input type="text" name="color" /><br/>
+              Is Ready To Eat: <input type="checkbox" name="readyToEat" /><br/>
+              <input type="submit" name="" value="Create Veggie"/>
+            </form>
+        </div>);
+    }
+  }
+
+module.exports = NewVeggie;
