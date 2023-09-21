f7cb3f56a0c6976f9cbb17a5739824e759ba5eaf
ehcodes <engrid.hernandez@gmail.com>
2023-09-21 06:37:31 -0400

    replaced local data reliance with db reliance

--- views/fruits/Index.jsx
+++ views/fruits/Index.jsx
@@ -2,23 +2,21 @@ const React = require("react");
 
 class Index extends React.Component {
   render() {
-    const { fruits } = this.props;
     return (
       <div>
         <nav>
           <a href="/fruits/new">Create a New Fruit</a>
-      </nav>
-        <h1>Fruits Index Page</h1>
+        </nav>
+        <h1>Fruits Index (MongoDB)</h1>
         <ul>
-          {fruits.map((fruit, i) => {
+          {this.props.fruits.map((fruit, i) => {
             return (
               <li>
-                The <a href={`/fruits/${i}`}>{fruit.name}</a> is {fruit.color}{" "}
-                <br></br>
+                The <a href={`/fruits/${fruit.id}`}> {fruit.name} </a> is{" "}
+                {fruit.color}
                 {fruit.readyToEat
                   ? `It is ready to eat`
                   : `It is not ready to eat`}
-                <br />
               </li>
             );
           })}
@@ -27,4 +25,5 @@ class Index extends React.Component {
     );
   }
 }
+
 module.exports = Index;
