bbdb0e9733c1fc82ad6fb8d35e09c72dfab243f6
ehcodes <engrid.hernandez@gmail.com>
2023-09-21 06:43:01 -0400

    created index page outside of fruits folder, will have future pertinence

---
+++ views/Index.jsx
@@ -0,0 +1,13 @@
+const React = require("react");
+
+class Index extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Fruits index page</h1>
+      </>
+    );
+  }
+}
+
+module.exports = Index;
