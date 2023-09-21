cb84b0c662059de8b5178cc45ea259b39b650b91
ehcodes <engrid.hernandez@gmail.com>
2023-09-21 08:45:08 -0400

    missed a variable

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.get("/veggies/:id", async function (req, res) {
   }
 });
 
-app.listen(localPort, () => {
+app.listen(3000, () => {
   console.log(`listening on port 3000`);
   console.log(`http://localhost:3000/`);
 });
